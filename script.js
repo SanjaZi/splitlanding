019c8215f7daa5f1643d9ea912991e84d51fdf3a
SanjaZi <80321654+SanjaZi@users.noreply.github.com>
2023-02-15 16:16:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const playstation = document.getElementById('playstation');
+const xbox = document.getElementById('xbox');
+
+
+xbox.addEventListener('mouseenter', (e) => {
+    xbox.style.flexGrow = '3';
+  });
+  
+  xbox.addEventListener('mouseleave', (e) => {
+    xbox.style.flexGrow = '1';
+  });
+
+  playstation.addEventListener('mouseenter', (e) => {
+    playstation.style.flexGrow = '3';
+  });
+  
+  playstation.addEventListener('mouseleave', (e) => {
+    playstation.style.flexGrow = '1';
+  });
+
+
+  xbox.addEventListener('touchstart', (event) => {
+    xbox.style.flexGrow = '3';
+    playstation.style.flexGrow = '1';
+    console.log("hey");
+  });
+
+
+  playstation.addEventListener('touchstart', (e) => {
+    playstation.style.flexGrow = '3';
+    xbox.style.flexGrow = '1';
+    console.log("hey2");
+  });
+  
+
